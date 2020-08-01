eddcc747b98504fcf84e72c05e445ea3aa479a2a
George Black <contact@georgeblack.me>
2020-08-01 11:53:15 -0500

    Update static content worker (#1)

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-import mime from "mime";
+const METHOD_NOT_ALLOWED_BODY = `
+<!DOCTYPE html>
+<html lang="en">
+  <body style="font-family: system-ui;text-align:center;">
+    <h1>METHOD NOT ALLOWED!</h1>
+    <p><em>What in tarnation are you trying to do?</em><p>
+  </body>
+</html>
+`;
+const NOT_FOUND_RESPONSE_BODY = `
+<!DOCTYPE html>
+<html lang="en">
+  <body style="font-family: system-ui;text-align:center;">
+    <h1>404 NOT FOUND!</h1>
+    <p><em>You better <a href="https://georgeblack.me" style="text-decoration: none;">head on home</a>, before you get into trouble.</em><p>
+  </body>
+</html>
+`;
+const INTERNAL_ERROR_RESPONSE_BODY = `
+<!DOCTYPE html>
+<html lang="en">
+  <body style="font-family: system-ui;text-align:center;">
+    <h1>INTERNAL SERVER ERROR, OH SHIT!</h1>
+    <p><em>Sorry 'bout that</em><p>
+  </body>
+</html>
+`;
 
-const ORIGIN = "https://media.georgeblack.me";
-const REMOTE_STORAGE_URL = "https://storage.googleapis.com/media.georgeblack.me";
-const CACHE_TTL = "7776000";
+const REMOTE_STORAGE_URL = "https://storage.googleapis.com";
 
 addEventListener("fetch", (event) => {
   try {
@@ -17,8 +41,7 @@ addEventListener("fetch", (event) => {
  */
 async function handleEvent(event) {
   let response;
-  const cache = caches.default;
-  const pathname = getPathname(event);
+  const key = getKey(event);
 
   if (event.request.method == "OPTIONS") {
     return new Response(null, {
@@ -30,21 +53,36 @@ async function handleEvent(event) {
   }
 
   if (event.request.method != "GET") {
-    return new Response("Method not allowed", { status: 405 });
+    return new Response(METHOD_NOT_ALLOWED_BODY, {
+      status: 405,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   }
 
   // check cache
-  response = await cache.match(`${ORIGIN}/${pathname}`);
+  response = await caches.default.match(event.request.url);
   if (response) return response;
 
   // fetch asset from cloud storage
-  let storageResponse = await fetch(`${REMOTE_STORAGE_URL}/${pathname}`);
+  let storageResponse = await fetch(`${REMOTE_STORAGE_URL}/${key}`);
 
   if (storageResponse.status == 404) {
-    return new Response("404 not found", { status: 404 });
+    return new Response(NOT_FOUND_RESPONSE_BODY, {
+      status: 404,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   }
   if (storageResponse.status != 200) {
-    return new Response("Internal error", { status: 500 });
+    return new Response(INTERNAL_ERROR_RESPONSE_BODY, {
+      status: 500,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   }
 
   // build and cache response
@@ -52,20 +90,19 @@ async function handleEvent(event) {
     headers: {
       "Access-Control-Allow-Origin": "https://georgeblack.me",
       "Access-Control-Allow-Methods": "GET, OPTIONS",
-      "Content-Type": mime.getType(pathname) || "text/plain",
-      "Cache-Control": `public, max-age=${CACHE_TTL}`,
+      "Content-Type":
+        storageResponse.headers.get("Content-Type") || "text/plain",
+      "Cache-Control": `public, max-age=${cacheMaxAge(key)}`,
     },
   });
-  event.waitUntil(cache.put(`${ORIGIN}/${pathname}`, response.clone()));
+  event.waitUntil(caches.default.put(event.request.url, response.clone()));
 
   return response;
 }
 
-/**
- * Get sanitized pathname of asset
- */
-function getPathname(event) {
-  let pathname = new URL(event.request.url).pathname;
+function getKey(event) {
+  let url = new URL(event.request.url);
+  let pathname = url.pathname;
   if (pathname.endsWith("/")) {
     pathname = pathname.concat("index.html");
   }
@@ -73,6 +110,13 @@ function getPathname(event) {
   if (!filename.includes(".")) {
     pathname = pathname.concat("/index.html");
   }
-  pathname = pathname.replace(/^\/+/, "");
-  return pathname;
+  return `${url.hostname}${pathname}`;
+}
+
+function cacheMaxAge(key) {
+  const extension = key.split(".").pop();
+  if (/^(jpg|jpeg|png|webp|mov|ico|svg|webmanifest)$/.test(extension))
+    return "1296000"; // 15 days
+  if (/^(js|css)$/.test(extension)) return "172800"; // 2 days
+  return "7200"; // 2 hours
 }
