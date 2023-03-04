d070882fad01bb11e073c675a06bc2bb51b39b53
George Black <contact@george.black>
2023-03-04 09:49:26 -0600

    Merge both Cloudflare workers into one

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-const METHOD_NOT_ALLOWED_BODY = `
-<!DOCTYPE html>
-<html lang="en">
-  <body style="font-family: system-ui;text-align:center;">
-    <h1>METHOD NOT ALLOWED!</h1>
-    <p><em>What in tarnation are you trying to do?</em><p>
-  </body>
-</html>
-`;
-
-const NOT_FOUND_RESPONSE_BODY = `
-<!DOCTYPE html>
-<html lang="en">
-  <body style="font-family: system-ui;text-align:center;">
-    <h1>404 NOT FOUND!</h1>
-    <p><em>You better <a href="https://george.black" style="text-decoration: none;">head on home</a>, before you get into trouble.</em><p>
-  </body>
-</html>
-`;
+import { MIME_TYPES } from "./constants.js";
 
 export default {
   async fetch(request, env, context) {
@@ -25,9 +7,22 @@ export default {
 };
 
 /**
- * Primary event handler
+ * Redirect requests to the correct handler.
  */
 async function handleEvent(request, env, context) {
+  const url = new URL(request.url);
+  if (url.hostname === env.PUBLIC_HOSTNAME) {
+    return handleStandardEvent(request, env, context);
+  } else if (url.hostname === env.STORAGE_HOSTNAME) {
+    return handleStorageEvent(request, env);
+  }
+  return new Response("Not found", { status: 404 });
+}
+
+/**
+ * Serve static assets from cache, otherwise read from R2 (and cache them).
+ */
+async function handleStandardEvent(request, env, context) {
   let response;
   const key = getKey(request);
 
@@ -41,30 +36,18 @@ async function handleEvent(request, env, context) {
   }
 
   if (request.method != "GET") {
-    return new Response(METHOD_NOT_ALLOWED_BODY, {
-      status: 405,
-      headers: {
-        "Content-Type": "text/html",
-      },
-    });
+    return new Response("Method not allowed", { status: 405 });
   }
 
-  // check cache
+  // Check cache
   response = await caches.default.match(request.url);
   if (response) return response;
 
-  // fetch from r2
+  // Fetch from r2
   const object = await env.WEB.get(key);
-  if (!object) {
-    return new Response(NOT_FOUND_RESPONSE_BODY, {
-      status: 404,
-      headers: {
-        "Content-Type": "text/html",
-      },
-    });
-  }
+  if (!object) return new Response("Not found", { status: 404 });
 
-  // build and cache response
+  // Build and cache response
   response = new Response(object.body, {
     headers: {
       "Access-Control-Allow-Origin": "https://george.black",
@@ -79,6 +62,44 @@ async function handleEvent(request, env, context) {
   return response;
 }
 
+/**
+ * Read and write data to R2.
+ */
+async function handleStorageEvent(request, env) {
+  if (!["GET", "PUT", "DELETE"].includes(request.method)) {
+    return new Response("Method not allowed", { status: 405 });
+  }
+  if (!authorized(request, env)) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  // List bucket objects
+  if (request.method === "GET") {
+    const objects = await env.WEB.list();
+    const keys = objects.objects.map((object) => object.key);
+    return new Response(JSON.stringify({ keys }), { status: 200 });
+  }
+
+  // Use URL to determine object key
+  // i.e. 'http://example.com/picture.jpg' -> 'picture.jpg'
+  if (request.method === "PUT") {
+    const pathname = new URL(request.url).pathname;
+    const objectKey = pathname.replace(/^\/+/, "");
+    await env.WEB.put(objectKey, request.body, {
+      httpMetadata: generateHttpMetadata(objectKey),
+    });
+    return new Response("Success", { status: 201 });
+  }
+
+  // Delete object, using URL to determine object key
+  if (request.method === "DELETE") {
+    const pathname = new URL(request.url).pathname;
+    const objectKey = pathname.replace(/^\/+/, "");
+    await env.WEB.delete(objectKey);
+    return new Response("Success", { status: 204 });
+  }
+}
+
 function getKey(request) {
   let url = new URL(request.url);
   let pathname = url.pathname;
@@ -90,4 +111,22 @@ function getKey(request) {
     pathname = pathname.concat("/index.html");
   }
   return pathname.substring(1); // removes leading slash
+}
+
+function authorized(request, env) {
+  const token = request.headers.get("X-Access-Token");
+  return token === env.ACCESS_TOKEN;
+}
+
+function generateHttpMetadata(objectKey) {
+  const extension = objectKey.split(".").pop();
+
+  let seconds = "2592000";
+  if (["html", "xml", "json", "txt"].includes(extension)) seconds = "900";
+  if (["css", "js"].includes(extension)) seconds = "172800";
+
+  return {
+    contentType: MIME_TYPES.get(extension) || "application/octet-stream",
+    cacheControl: `public, max-age=${seconds}`,
+  };
 }
